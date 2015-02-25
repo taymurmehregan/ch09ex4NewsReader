12cd8c1ab3bd96ee29d2c722a7a81deb3ab5c56b
Taymur Mehregan <89680@CIS-25-74.coleman.local>
2015-02-24 19:21:18 -0800

    Initial commit

---
+++ app/src/main/java/com/murach/newsreader/ItemActivity.java
@@ -0,0 +1,33 @@
+package com.murach.newsreader;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+
+public class ItemActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // get the intent
+        Intent intent = getIntent();
+
+        // get the Uri for the link
+        String link = intent.getStringExtra("link");
+        Uri viewUri = Uri.parse(link);
+
+        // create the intent and start it
+        Intent viewIntent = new Intent(Intent.ACTION_VIEW, viewUri);
+        startActivity(viewIntent);
+    }
+}
